ade8dcdc69414ee10703e1200519696e410175c3
Md-Rakib55 <mdobaedullahrakib51055@gmail.com>
2025-06-20 21:11:03 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+   <h1 className="text-4xl font-bold underline bg-green-500 px-5">
+    Hello world!
+  </h1>
+    </>
+  )
+}
+
+export default App
